f6a4d6b8b431fd2299fe715e2e04c05e9330ef41
Andrey Zhukov <settler59@yandex.ru>
2024-05-07 17:33:30 +0200

    feature: страница условия доставки

--- components/CartContacts.tsx
+++ components/CartContacts.tsx
@@ -1,30 +1,36 @@
-import React from 'react';  
-import { useForm } from 'react-hook-form';
-import InputMask from 'react-input-mask';
+import React from "react";
+import { useForm } from "react-hook-form";
+import InputMask from "react-input-mask";
 // import './Profile.css';
 // import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import styles from "../styles/Cart.module.css";
 
 interface cartContactsProps {
   isDelivery: boolean;
+  passErrors?: (isValid: boolean, errors: string[]) => void;
+  passContacts?: (contacts: { [key: string]: string }) => void;
+  isSubmiyClicked: boolean;
+  // isTimeToReset: boolean // DEL?
 }
 
-const CartContacts: React.FC<cartContactsProps> = ({ isDelivery }) => {
-
+const CartContacts: React.FC<cartContactsProps> = ({ isDelivery, passErrors, passContacts, isSubmiyClicked }) => {
   // const { isLoggedIn, currentUser, onProfile, onNavMenuClick, onExitProfile } = props;
-  
   // const [isDataNew, setIsDataNew] = React.useState(false);
   const {
     register,
     formState: { errors, isValid, isDirty },
-    handleSubmit,
+    getValues,
+    // handleSubmit,
     reset,
-    watch
+    // watch,
+    trigger
   } = useForm({
     // defaultValues: {profileName: currentUser.name, profileEmail: currentUser.email},
-    mode: "all" // "onChange"
+    // mode: "all",
+    mode: "onChange"
   });
-  
+
   // useEffect(()=>{
   //   const compare = watch((data) => {
   //     if (data.profileName === data.name && data.profileEmail === data.email) {
@@ -35,80 +41,185 @@ const CartContacts: React.FC<cartContactsProps> = ({ isDelivery }) => {
   //   })
   // },[watch])
 
-
-  const onSubmit = (data) => {  
+  // const onSubmit = (data) => {
     // onProfile(data.profileName, data.profileEmail);
     // reset(currentUser);
+  // };
+
+  // useEffect(() => {
+  //   console.log(isValid);
+  //   // 
+  // }, [isValid])
+
+  // async function test() {
+  const test = async () => {
+    // console.log(getValues());
+    // const errorsArray = []
+    // const nameError = errors.name?.message;
+    // const phoneError = errors.phone?.message;
+    // const emailError = errors.email?.message;
+    // const addressError = errors.address?.message;
+    console.log(errors)
+    const errorsArray = [
+      errors.name && errors.name.message,
+      errors.phone && errors.phone.message,
+      errors.email && errors.email.message,
+      errors.adress && errors.adress.message
+    ].filter(Boolean);
+
+    // console.log(errors.name);
+    const check = await trigger(); // Возвращает true, если форма валидна
+    if (!check) {
+      console.log(errors); // Выводит объект ошибок в консоль
+      console.log(errorsArray)
+    }
   }
 
-  function test() {
-    console.log(isValid)
-  }
+  useEffect(()=> {
+    // console.log('а здесь были')
+    if (!isValid) {
+      return
+    } else if (isSubmiyClicked) {
+      // console.log('здесь были')
+      // console.log(getValues());
+      const data = getValues()
+      passContacts(data);
+      reset();
+    } else {
+      return;
+    }
+  }, [isSubmiyClicked])
+
+  useEffect(() => {
+    if (!isValid) {
+      // console.log('вызываем при невалидности')
+      const errorsArray = [
+        errors.name && errors.name.message,
+        errors.phone && errors.phone.message,
+        errors.email && errors.email.message,
+        isDelivery ? errors.adress && errors.adress.message : {}
+      ].filter(Boolean);
+      passErrors(isValid, errorsArray.map(error => typeof error === 'string' ? error : ''));
+    } else {
+      // console.log('вызываем при валидности')
+      // const contacts = getValues()
+      passErrors(isValid, []);
+      // passContacts(getValues())
+    }
+  }, [isValid, errors.name?.message, errors.phone?.message, errors.email?.message, errors.adress?.message, isDelivery])
+
+  useEffect(() => {
+    trigger();
+  }, [])
+
+  // useEffect(() => {
+  //   console.log(errors);
+  // }, [errors])
+
+
+  // useEffect(() => {
+  //   console.log(`'принудительяная проверка' ${errors}`)
+  // }, [errors])
 
   return (
-    <>
-      <section className="profile">
-        {/* <h2 className="profile__title" onClick={test}>Привет, {currentUser.name}</h2> */}
-        <form id="profileForm" name="profileForm" className="profile__form" onSubmit={handleSubmit(onSubmit)}>
-          <label className="profile__inputTitle">
-            Имя
-            <input 
-              {... register("profileName", {
-                required: {
-                  value: true,
-                  message: "Поле Имя обязательно к заполнению"}
-              })}
-              type="text" 
-              // placeholder={currentUser.name} 
-              placeholder='Имя' 
-              className="profile__input" 
-            />
-          </label>
+    <form
+      id="customerData"
+      name="customerData"
+      className={styles.cart__customerDataForm}
+      // onSubmit={handleSubmit(onSubmit)}
+    >
+      <h2 className={styles.cart__title}
+        onClick={test}
+      >Данные получателя</h2>
+      {/* <label className="profile__inputTitle"> */}
+        {/* Имя */}
+        <input
+          {...register("name", {
+            required: {
+              value: true,
+              message: "Заполните поле Имя",
+            },
+          })}
+          type="text"
+          placeholder="Имя и фамилия *"
+          className={styles.cart__customerData}
+        />
+      {/* </label> */}
 
-          <label className='profile__account-label'>Номер телефона</label>
-            <InputMask
-              minLength={16}
-              alwaysShowMask={true}
-              name='phone'
-              // onBlur={formik.handleBlur}
-              // onChange={formik.handleChange}
-              className='profile__account-input'
-              mask={'+7(999)999-99-99'}
-              placeholder='+7(___)___-__-__'
-            />
-            {/* {formik.touched.phone != null && formik.errors.phone != null && (
-              <span className='profile__account-error'>
-                {formik.errors.phone}
-              </span>
-            )} */}
-
-          <label className="profile__inputTitle" >E-mail  
-            <input 
-              {... register("profileEmail", {
-                required: "Поле Email обязательно к заполнению",
-                pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: "Некорректный адрес электронной почты"
-                }
-              })}
-              type="email" 
-              placeholder='email' 
-              className="profile__input" 
-            />
-          </label>
-          
-          {/* {errors.profileName && <span className="profile__error">{errors?.profileName?.message || "Другая ошибка"}</span>}
-          {errors.profileEmail && <span className="profile__error">{errors?.profileEmail?.message || "Другая ошибка"}</span>} */}
-          <div className="profile__buttonBox">
-            {/* <button disabled={!isValid || !isDataNew || !isDirty} type="submit" className="profile__button">Редактировать</button>
-            <Link onClick={onExitProfile} to={'./'} className="profile__link">Выйти из аккаунта</Link> */}
-          </div>
-        </form>
+      {/* <label className="profile__account-label"></label> */}
+      <InputMask
+        {...register("phone", {
+          required: {
+            value: true,
+            message: "Заполните поле Телефон",
+          },
+          pattern: {
+            value: /^(\+7|8)\(\d{3}\)\d{3}-\d{2}-\d{2}$/,
+            message: "Введите корректный номер телефона",
+          },
+        })}
+        // onChange={handlePhoneChange}
+        // minLength={10}
+        alwaysShowMask={false}
+        name="phone"
+        className={styles.cart__customerData}
+        mask="+7(999)999-99-99"
+        placeholder="Телефон *"
         
+      />
+        <input
+          {...register("email", {
+            required: "Заполните поле Email",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "Некорректный адрес электронной почты",
+            },
+          })}
+          type="email"
+          placeholder="email *"
+          className={styles.cart__customerData}
+        />
+      {isDelivery && (
+        <textarea
+          {...register("adress", {
+            required: {
+              value: true,
+              message: "Заполните поле Адрес",
+            },
+          })}
+          // type="text"
+          placeholder="Адрес доставки *"
+          className={styles.cart__customerData}
+        />
+      )}
       
-      </section>
-    </>
-  )
-}
+        <textarea
+          {...register("comment", {
+            required: false
+          })}
+          // type="text"
+          placeholder="Комментарий к заказу"
+          className={styles.cart__customerData}
+        />
+    </form>
+  );
+};
 
 export default CartContacts;
+
+
+{/* <InputMask
+        // maskChar='2'
+        minLength={16}
+        alwaysShowMask={false}
+        name="phone"
+        // onBlur={formik.handleBlur}
+        // onChange={formik.handleChange}
+        className={styles.cart__customerData}
+        mask={"+7(999)999-99-99"}
+        placeholder="Телефон"
+        // placeholderChar='Телефон'
+        // showMask={true}
+        // maskPlaceholder={null} value="телефон"
+        // maskPlaceholder={'телефон'}
+      /> */}
---
+++ pages/delivery.tsx
@@ -0,0 +1,81 @@
+import SectionLine from '../components/SectionLine'
+import styles from '../styles/Delivery.module.css'
+import { mapSrc } from '../utils/constatnts'
+
+export default function delivery () {
+  return (
+    <>
+      <section className={styles.delivery}>
+        <div className={styles.delivery__chapter}>
+          <h2 className={styles.delivery__title}>Доставка и оплата</h2>
+          <p className={styles.delivery__text}>
+            Мы доставим кофе до вашей двери в день заказа, если вы находитесь в Санкт-Петербурге. Оплата происходит на сайте банковской картой MasterCard, Visa, Maestro, Мир.
+          </p>
+          <p className={styles.delivery__text}>
+            При заказе до 18:00 мы доставим кофе в день заказа. При заказе после 18:00, на следующий день. 
+          </p>
+        </div>
+        <div className={styles.delivery__chapter}>
+          <h2 className={styles.delivery__title}>Стоимость доставки</h2>
+          <p className={styles.delivery__text}>
+            Самовывоз в любой из наших кофеен бесплатно.
+          </p>
+          <ul className={`${styles.delivery__text} ${styles.delivery__text_type_listTitle}`}>
+            Доставка по Санкт-Петербургу в пределах зеленой зоны на карте:
+            <li className={styles.delivery__text_type_listItem}>- бесплатно при заказе на сумму от 1800р.</li>
+            <li className={styles.delivery__text_type_listItem}>- 400р при заказе менее 1800р.</li>
+          </ul>
+          <p className={styles.delivery__text}>
+            Доставка в другие города РФ и доставка по Санкт-Петербургу за пределами зеленой зоны осуществляется компанией СДЭК. В этом случае доставка оплачивается отдельно при получении. Мы отвозим на склад СДЭК в Санкт-Петербурге бесплатно, а вы оплачиваете услуги транспортной компании при получении кофе.
+          </p>
+          <p className={styles.delivery__text}>
+            Обычно стоимость доставки до пункта выдачи СДЭК стоит 200-300 рублей, а до квартиры 400-500 рублей. Мы свяжемся с вами, скажем точную стоимость и вы выберете вариант доставки.
+          </p>
+          
+        </div>
+        <div className={styles.delivery__chapter}>
+          <h2 className={styles.delivery__title}>Зоны доставки</h2>
+          <p className={styles.delivery__text}>
+            При заказе в пределах зеленой зоны Санкт-Петербурга мы отправляем кофе курьерской службой.
+          </p>
+          <p className={styles.delivery__text}>
+            При заказе за пределами зеленой зоны доставка осуществляется компанией СДЭК.
+          </p>
+          <div className={styles.delivery__map} >
+              <iframe
+                className={styles.delivery__iframe}
+                src={mapSrc}
+                // title="2"
+              ></iframe>
+            </div>
+        </div>
+
+        <div className={styles.delivery__chapter}>
+          <h2 className={styles.delivery__title}>Самовывоз</h2>
+          <ul className={`${styles.delivery__text} ${styles.delivery__text_type_listTitle}`}>
+            Вы можете забрать заказ из наших кофеен:
+            <li className={styles.delivery__text_type_listItem}>- PITCHER на Марата 2 работает по графику понедельник-пятница с 9 до 22, суббота-воскресенье с 10 до 22.</li>
+            <li className={styles.delivery__text_type_listItem}>- PITCHER на Бассейной 12 работает по графику понедельник-пятница с 8 до 22, суббота с 9 до 22, воскресенье с 10 до 22.</li>
+          </ul>
+        </div>
+        <div className={styles.delivery__chapter}>
+          <h2 className={styles.delivery__title}>Возврат</h2>
+          <p className={styles.delivery__text}>
+            Мы уверены в высоком качестве нашего кофе, но по закону обязаны рассказать, как вы можете его вернуть.
+          </p>
+          <p className={styles.delivery__text}>
+            Согласно Закону РФ «О защите прав потребителей» вернуть продукты питания, в том числе кофе, можно в случае если продукция не соответствует стандартам или санитарным нормам: истек срок годности; продукт испортился и стал непригодным для употребления в пищу; в упаковке находится содержимое, не соответствующее описанию; обнаружено наличие посторонних предметов.
+          </p>
+          <p className={styles.delivery__text}>
+            Если вы обнаружили один из этих признаков в кофе, свяжитесь с нами и мы вернем вам деньги.
+          </p>
+          <p className={styles.delivery__text}>
+          </p>
+        </div>
+      </section>
+    <SectionLine/>
+    </>
+  )
+}
+
+// 
