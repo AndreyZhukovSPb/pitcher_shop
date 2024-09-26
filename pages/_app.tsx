6d2be42ca3012c3251409da061c0b0ff94c7fc04
Andrey Zhukov <settler59@yandex.ru>
2024-09-26 12:05:50 +0200

    fix: правлю каунтер для айфона 3 + скрол хедер для айфон

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,8 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <title>Specialty coffee</title>
         <link rel="icon" href="/favicon.ico/" sizes="any" lang="RU"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
       </Head>
       <ScrollToTop />
       <div className='page'>
